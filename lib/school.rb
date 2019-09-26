b483bd94275f03a04d691060143aa70db03695af
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-26 22:13:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,11 @@
 class School
-  attr_reader :name, :roster
+  attr_reader :name
   def initialize(name)
     @name = name
   end
   
-  @roster = {}
+  def roster=(roster)
+    @roster = roster
   
   def roster
     @roster
